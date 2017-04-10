edd6f61d1913d7879379b95cd07d826043d39a55
Muhamad Diaz <muhamaddiaz10@gmail.com>
2017-04-11 01:10:07 +0700

    Pass 1

--- js/kangguru.js
+++ js/kangguru.js
@@ -1,4 +1,6 @@
 $(function() {
+  var oText = $('.sambut').html();
+  var oTextSub = $('.sambut-sub').text();
   $(window).scroll(function() {
     var wScroll = $(window).scrollTop();
     if(wScroll > 0) {
@@ -6,6 +8,14 @@ $(function() {
     } else {
       $('.navigasi').removeClass('menu-putih');
     }
-    $('.sambut-group').css('transform', 'translateY('+ (100 + wScroll / 2.5) +'%)');
+    $('.sambut-group').css('transform', 'translateY('+ (100 + wScroll / 2) +'%)');
+    if(wScroll > 2000)
+    {
+      $('.sambut').html('<span class="kurawal">{</span>Coded with <span style="color: orange"><span class="fa fa-heart"></span></span> By<span class="kurawal">}</span>');
+      $('.sambut-sub').html('Muhamad Diaz');
+    } else {
+      $('.sambut').html(oText);
+      $('.sambut-sub').html(oTextSub);
+    }
   });
 });
